509c72c7e19afc23197d80ca88a68a93fdc6009b
Mandal <2463363@cognizant.com>
2026-01-12 15:04:25 +0530

    fixed btn spacing, added some css, added active class using binding

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -7,12 +7,14 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  activeComponent:string = "dashboard"
 // 1. Create the Output decorator (the "speaker")
   @Output() navClick = new EventEmitter<string>();
 
   // 2. Function called when a button is clicked
   onNavigate(view: string) {
     // 3. Emit the value ('dashboard' or 'history') up to the parent
+    this.activeComponent=view;
     this.navClick.emit(view);
   }
 }
